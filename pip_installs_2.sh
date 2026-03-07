f67a1acb0cef93a8fcb977291941ac298d374795
aaronlolo326 <aaronlo326@gmail.com>
2026-03-07 07:01:19 +0000

    update pip script

--- pip_installs_2.sh
+++ pip_installs_2.sh
@@ -6,12 +6,12 @@ pip install pyarrow
 pip install transformers
 pip install safetensor
 pip install einops
-pip install torch==2.6.0
-pip install torchvision==0.21.0
+pip install torch==2.7.0
+pip install torchvision==0.22.0
 
 # flash attention
-pip install ~/flash_attn-2.7.4.post1+cu12torch2.6cxx11abiFALSE-cp311-cp311-linux_x86_64.whll
-
+# pip install ~/flash_attn-2.7.4.post1+cu12torch2.6cxx11abiFALSE-cp311-cp311-linux_x86_64.whl
+pip install ~/flash_attn-2.7.4.post1+cu12torch2.7cxx11abiFALSE-cp311-cp311-linux_x86_64.whl
 pip install flash-linear-attention
 
 pip install datasets==4.5.0
