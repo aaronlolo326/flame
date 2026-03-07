9b14612cbcf23f8ce8d0868bad5e30696cb5b675
aaronlolo326 <aaronlo326@gmail.com>
2026-03-07 07:01:38 +0000

    fix param cnt script

--- scripts/param_cnt.py
+++ scripts/param_cnt.py
@@ -13,12 +13,16 @@ def main(job_config: JobConfig):
     with torch.device("meta"):
         model = AutoModelForCausalLM.from_config(model_config)
     # calculate model size and flops per token
-    model_param_count, nparams_embedding, num_flops_per_token = get_nparams_and_flops(
+    num_distinct_params, nparams_embedding, num_flops_per_token = get_nparams_and_flops(
         model, model_config, job_config.training.context_len
     )
-    print(f"Number of parameters = {model_param_count:,}")
+    print(f"Number of distinct model parameters = {num_distinct_params:,}")
+    if model_config.tie_word_embeddings:
+        print("Model ties input and output word embeddings, so")
+        print(f"Number of total (non-distinct) parameters = {num_distinct_params + nparams_embedding:,}")
+    else:
+        print(f"Model does not tie input and output word embeddings, so the number of unique model parameters is the above.")
     print(f"Number of embedding parameters = {nparams_embedding:,}")
-    print(f"Number of parameters w/o embedding parameters = {model_param_count - nparams_embedding:,}")
     print(f"Number of flops per token = {num_flops_per_token:,}")
 
 if __name__ == '__main__':
